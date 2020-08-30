6f879bd8f3f002465ea3a62620f55ba98252e54a
Eric <34190213+epodol@users.noreply.github.com>
2020-08-30 09:04:49 -0700

    Added Coin Command
    Signed-off-by: Eric <34190213+epodol@users.noreply.github.com>

--- src/main/java/dev/erictech/bearbot/commands/fun/games/CoinCommand.java
+++ src/main/java/dev/erictech/bearbot/commands/fun/games/CoinCommand.java
@@ -1,18 +1,28 @@
 package dev.erictech.bearbot.commands.fun.games;
 
-import dev.erictech.bearbot.ComingSoonError;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.javacord.api.event.message.MessageCreateEvent;
-import org.javacord.api.listener.message.MessageCreateListener;
+import de.btobastian.sdcf4j.Command;
+import de.btobastian.sdcf4j.CommandExecutor;
+import org.javacord.api.DiscordApi;
+import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.message.Message;
+import org.javacord.api.entity.message.MessageBuilder;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
+import org.javacord.api.entity.server.Server;
 
-public class CoinCommand implements MessageCreateListener {
-    static final Logger logger = LogManager.getLogger(CoinCommand.class.getName());
+import java.util.Random;
 
-    public void onMessageCreate(MessageCreateEvent event) {
-        if (event.getMessageContent().equalsIgnoreCase("!coin")) {
-            logger.info(event.getMessage() + " in " + event.getServer() + " " + event.getChannel() + " by user " + event.getMessageAuthor());
-            ComingSoonError.commingSoon(event.getChannel());
-        }
+public class CoinCommand implements CommandExecutor {
+    @Command(aliases = {"!coin", "!flip", "!flipacoin", "!coinflip"}, description = "Flips a coin", usage = "!coin")
+    public void onCommand(DiscordApi api, Message message, TextChannel channel, Server server, String[] args) {
+        Random rand = new Random();
+        String result;
+        boolean coin = rand.nextBoolean();
+        if (coin) result = "Heads";
+        else result = "Tails";
+        new MessageBuilder().setEmbed(new EmbedBuilder()
+                .setTitle("Flip a coin!")
+                .addField("Result: ", "It was **" + result + "**!")
+                .setFooter("Requested by: " + message.getAuthor().getDisplayName(), message.getAuthor().getAvatar()))
+                .send(channel);
     }
 }
