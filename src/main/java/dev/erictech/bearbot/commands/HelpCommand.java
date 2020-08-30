49cb8687d8a4fddbc524f051524ab3a90199bc66
Eric <34190213+epodol@users.noreply.github.com>
2020-08-30 10:30:10 -0700

    Added slf4J and "Requested by" footer Made commands appear in code blocks Removed @Bear Added !version
    Signed-off-by: Eric <34190213+epodol@users.noreply.github.com>

--- src/main/java/dev/erictech/bearbot/commands/HelpCommand.java
+++ src/main/java/dev/erictech/bearbot/commands/HelpCommand.java
@@ -2,26 +2,28 @@ package dev.erictech.bearbot.commands;
 
 import de.btobastian.sdcf4j.Command;
 import de.btobastian.sdcf4j.CommandExecutor;
+import org.javacord.api.DiscordApi;
+import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.message.MessageBuilder;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
-import org.javacord.api.event.message.MessageCreateEvent;
+import org.javacord.api.entity.server.Server;
 
 import java.awt.*;
 
 public class HelpCommand implements CommandExecutor {
 
     @Command(aliases = {"!help"}, description = "Returns a random meme from reddit", usage = "!help")
-    public void onMessageCreate(MessageCreateEvent event) {
-        if (event.getMessageContent().equalsIgnoreCase("!help")) {
+    public void onCommand(DiscordApi api, Message message, TextChannel channel, Server server, String[] args) {
             new MessageBuilder().setEmbed(new EmbedBuilder()
                     .setTitle("Bear Bot Commands!")
                     .setDescription("Bear Bot is still a work in progress! Most of these commands may not work as intended!")
-                    .addInlineField("General", "!help, !ping, @bear")
-                    .addInlineField("Games", "!coin, !dice, !rps")
-                    .addInlineField("Pictures", "!meme, !dog")
-                    .addInlineField("Moderation", "!ban, !unban, !mute, !unmute, !kick")
-                    .setColor(Color.cyan))
-                    .send(event.getChannel());
-        }
+                    .addInlineField("General", "`!help`, `!ping`, `!version`")
+                    .addInlineField("Games", "`!coin`, `!dice`, `!rps`")
+                    .addInlineField("Pictures", "`!meme`, `!dog`")
+                    .addInlineField("Moderation", "`!ban`, `!unban`, `!mute`, `!unmute`, `!kick`, `!warn`")
+                    .setColor(Color.cyan)
+                    .setFooter("Requested by: " + message.getAuthor().getDisplayName(), message.getAuthor().getAvatar()))
+                    .send(channel);
     }
 }
