29b6da0475af57193fee27091610f6d97464ea2e
Eric <34190213+epodol@users.noreply.github.com>
2020-08-30 09:08:32 -0700

    Added slf4j and moved Ping listener to Main.java
    Signed-off-by: Eric <34190213+epodol@users.noreply.github.com>

--- src/main/java/dev/erictech/bearbot/Main.java
+++ src/main/java/dev/erictech/bearbot/Main.java
@@ -1,12 +1,13 @@
 package dev.erictech.bearbot;
 
+import de.btobastian.sdcf4j.CommandHandler;
+import de.btobastian.sdcf4j.handler.JavacordHandler;
 import dev.erictech.bearbot.commands.HelpCommand;
 import dev.erictech.bearbot.commands.PingCommand;
-import dev.erictech.bearbot.commands.PingSock;
 import dev.erictech.bearbot.commands.fun.games.CoinCommand;
 import dev.erictech.bearbot.commands.fun.games.DiceCommand;
 import dev.erictech.bearbot.commands.fun.games.RPSCommand;
-import dev.erictech.bearbot.commands.fun.pictures.DogPictureCommand;
+import dev.erictech.bearbot.commands.fun.pictures.DogCommand;
 import dev.erictech.bearbot.commands.fun.pictures.MemeCommand;
 import dev.erictech.bearbot.commands.moderation.*;
 import org.apache.logging.log4j.LogManager;
@@ -33,19 +34,30 @@ public class Main {
 
         logger.info("You can invite me by using the following url: " + api.createBotInvite());
 
-        api.addMessageCreateListener(new BanCommand());
-        api.addMessageCreateListener(new KickCommand());
-        api.addMessageCreateListener(new MuteCommand());
-        api.addMessageCreateListener(new UnMuteCommand());
-        api.addMessageCreateListener(new UnBanCommand());
-        api.addMessageCreateListener(new PingSock());
-        api.addMessageCreateListener(new PingCommand());
-        api.addMessageCreateListener(new HelpCommand());
-        api.addMessageCreateListener(new CoinCommand());
-        api.addMessageCreateListener(new DiceCommand());
-        api.addMessageCreateListener(new RPSCommand());
-        api.addMessageCreateListener(new DogPictureCommand());
-        api.addMessageCreateListener(new MemeCommand());
+        CommandHandler handler = new JavacordHandler(api);
+
+        handler.registerCommand(new HelpCommand());
+        handler.registerCommand(new PingCommand());
+        handler.registerCommand(new BanCommand());
+        handler.registerCommand(new KickCommand());
+        handler.registerCommand(new MuteCommand());
+        handler.registerCommand(new UnBanCommand());
+        handler.registerCommand(new UnMuteCommand());
+        handler.registerCommand(new WarnCommand());
+        handler.registerCommand(new CoinCommand());
+        handler.registerCommand(new DiceCommand());
+        handler.registerCommand(new RPSCommand());
+        handler.registerCommand(new MemeCommand());
+        handler.registerCommand(new DogCommand());
+
+
+
+        //Ping Sock
+        api.addMessageCreateListener(event -> {
+            if (event.getMessageContent().contains("<@!" + api.getClientId() + ">")) {
+                event.getMessage().addReaction(":PingSock:741719911679918182");
+            }
+        });
 
         api.addServerJoinListener(event -> logger.info("Joined server " + event.getServer().getName()));
         api.addServerLeaveListener(event -> logger.info("Left server " + event.getServer().getName()));
