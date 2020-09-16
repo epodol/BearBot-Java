d2bc0e5969d09a9d2a38925b4570299823b454a6
Eric <34190213+epodol@users.noreply.github.com>
2020-09-16 07:25:07 -0700

    Manual Console Start and Update PingSock
    Signed-off-by: Eric <34190213+epodol@users.noreply.github.com>

--- src/main/java/dev/erictech/bearbot/Main.java
+++ src/main/java/dev/erictech/bearbot/Main.java
@@ -1,3 +1,15 @@
+
+
+/**
+ * The BearBot program is a Java Discord Bot using
+ * Gradle and JavaCord that preforms most common
+ * bot commands.
+ *
+ * @author @epodol
+ * @version 0.1.4-alpha
+ * @since 2020-09-12
+ */
+
 package dev.erictech.bearbot;
 
 import de.btobastian.sdcf4j.CommandHandler;
@@ -8,6 +20,7 @@ import dev.erictech.bearbot.commands.VersionCommand;
 import dev.erictech.bearbot.commands.fun.games.CoinCommand;
 import dev.erictech.bearbot.commands.fun.games.DiceCommand;
 import dev.erictech.bearbot.commands.fun.games.RPSCommand;
+import dev.erictech.bearbot.commands.fun.pictures.CatCommand;
 import dev.erictech.bearbot.commands.fun.pictures.DogCommand;
 import dev.erictech.bearbot.commands.fun.pictures.MemeCommand;
 import dev.erictech.bearbot.commands.moderation.*;
@@ -17,23 +30,32 @@ import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.javacord.api.entity.activity.ActivityType;
 
+import java.util.Scanner;
+
 public class Main {
+
+    protected static ActivityType activityType = ActivityType.LISTENING;
+
+    protected static String activityName = "@Bear help";
 
     private static Logger logger = LogManager.getLogger(Main.class);
 
+    /**
+     * This is the main method which simply displays "Hello World" to the standard output.
+     *
+     * @param args Bot Token
+     */
+
     public static void main(String[] args) {
-        if (args.length < 1) {
-            System.err.println("Please provide a valid token as the first argument!");
-            return;
-        }
 
-        String token = args[0];
+//        BearBotProperties properties = new BearBotProperties();
+//        properties.getPropValues();
 
-        DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
+        startCommands(args.length);
+        DiscordApi api = startBot(args[0]);
 
-        api.updateActivity(ActivityType.LISTENING, " !help");
 
-        logger.info("You can invite me by using the following url: " + api.createBotInvite());
+//        logger.info("You can invite me by using the following url: " + api.createBotInvite());
 
         CommandHandler handler = new JavacordHandler(api);
 
@@ -51,12 +73,12 @@ public class Main {
         handler.registerCommand(new RPSCommand());
         handler.registerCommand(new MemeCommand());
         handler.registerCommand(new DogCommand());
-
+        handler.registerCommand(new CatCommand());
 
 
         //Ping Sock
         api.addMessageCreateListener(event -> {
-            if (event.getMessageContent().contains("<@!" + api.getClientId() + ">")) {
+            if (event.getMessage().getMentionedUsers().contains(api.getYourself())) {
                 event.getMessage().addReaction(":PingSock:741719911679918182");
             }
         });
@@ -64,4 +86,91 @@ public class Main {
         api.addServerJoinListener(event -> logger.info("Joined server " + event.getServer().getName()));
         api.addServerLeaveListener(event -> logger.info("Left server " + event.getServer().getName()));
     }
+
+    public static void startCommands(int argsLength) {
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("\n\n" +
+                "  ____                  ____        _   \n" +
+                " |  _ \\                |  _ \\      | |  \n" +
+                " | |_) | ___  __ _ _ __| |_) | ___ | |_ \n" +
+                " |  _ < / _ \\/ _` | '__|  _ < / _ \\| __|\n" +
+                " | |_) |  __/ (_| | |  | |_) | (_) | |_ \n" +
+                " |____/ \\___|\\__,_|_|  |____/ \\___/ \\__|\n" +
+                "                                        \n" +
+                "                                        \n");
+
+        System.out.println("Now Starting:");
+        if (argsLength != 1) {
+            System.err.println("Please provide a token as the first argument!");
+            return;
+        }
+        System.out.print("Enter \"0\" to use quickstart, otherwise enter \"1\":\t");
+        if (scn.nextByte() == 1) {
+            manualStart();
+        } else {
+            System.out.println("Quick Start!");
+        }
+    }
+
+    public static void manualStart() {
+
+        System.out.println("Manual Start!");
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("\nChoose one to change or enter \"0\" to exit Manual Start.");
+        System.out.println("\n#\tType\t\t\tCurrent Value");
+        System.out.println("\n1\tActivity Type\t" + activityType + "\n2\tActivity Name\t" + activityName + "\n2\tUpdate Activity");
+
+        switch (scn.nextByte()) {
+            case 0:
+                return;
+            case 1:
+                changeActivityTypeConsole();
+                break;
+            case 2:
+                System.out.println("Manual Start!");
+                changeActivityNameConsole();
+                break;
+            default:
+                System.out.println("Option Unknown!");
+                manualStart();
+                break;
+        }
+
+
+    }
+
+    public static void changeActivityTypeConsole() {
+
+        System.out.println("Change Activity Type");
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("\nChoose one to change or enter \"0\" to go back.");
+        System.out.println("\n#\tValue");
+        System.out.println("\n1\tPLAYING\n2\tSTREAMING\n3\tLISTENING\n4\tWATCHING");
+        int activity = scn.nextInt();
+        if (activity == 1 || activity == 2 || activity == 3 || activity == 4) {
+            activityType = ActivityType.getActivityTypeById(activity - 1);
+            System.out.println("\nSet the Activity Type to " + activityType + "\n");
+        } else {
+            System.out.println("Going Back");
+        }
+        manualStart();
+    }
+
+    public static void changeActivityNameConsole() {
+
+    }
+
+
+    public static DiscordApi startBot(String token) {
+        DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
+        api.updateActivity(activityType, activityName);
+        return api;
+    }
+
 }
